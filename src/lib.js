499a3570be5963b09c4c0a0e6dc10fa828322330
Andreas Philippi <18646892+andreasphil@users.noreply.github.com>
2024-11-16 17:26:36 +0100

    feat: add helper to render SVGs to HTML element

--- src/lib.js
+++ src/lib.js
@@ -77,6 +77,17 @@ import { C8, html } from "@andreasphil/c8";
 const frown =
   '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-frown"><circle cx="12" cy="12" r="10"/><path d="M16 16s-1.5-2-4-2-4 2-4 2"/><line x1="9" x2="9.01" y1="9" y2="9"/><line x1="15" x2="15.01" y1="9" y2="9"/></svg>';
 
+/**
+ * Takes an SVG string and converts it into an HTML element. Useful for
+ * displaying icons in the command bar.
+ *
+ * @param {string} svg
+ * @returns {HTMLElement}
+ */
+export function renderSvgFromString(svg) {
+  return new DOMParser().parseFromString(svg, "image/svg+xml").documentElement;
+}
+
 /* -------------------------------------------------- *
  * Functionality                                      *
  * -------------------------------------------------- */
